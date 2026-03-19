8d7fdaf114e96f5cba59dc659edf8dd16aad0bc3
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-19 10:53:46 +0100

    wpdate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 
 const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const consonants = 'BCDFGHJKLMNPQRSTVWXYZ'
-const vowels = 'AEIOU'
 
 function randomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
