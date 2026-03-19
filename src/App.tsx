6bdb7c1945a4f0026b8956a2f0cf7556a6c5cc0c
Kriszchan <panyik.krisztian@students.jedlik.eu>
2026-03-19 10:51:41 +0100

    qpdate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const consonants = 'BCDFGHJKLMNPQRSTVWXYZ'
+const vowels = 'AEIOU'
 
 function randomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
@@ -9,13 +11,16 @@ function randomInt(min: number, max: number): number {
 function randomLetters(length: number): string {
   return Array.from({ length }, () => LETTERS[randomInt(0, LETTERS.length - 1)]).join('')
 }
+function randomConsonants(length: number): string {
+  return Array.from({ length }, () => consonants[randomInt(0, consonants.length - 1)]).join('')
+}
 
 function randomDigits(length: number): string {
   return Array.from({ length }, () => randomInt(0, 9).toString()).join('')
 }
 
 function generateModernPlate(): string {
-  return `${randomLetters(2)}   ${randomLetters(2)}-${randomDigits(3)}`
+  return `${randomConsonants(2)}   ${randomLetters(2)}-${randomDigits(3)}`
 }
 
 
@@ -27,23 +32,32 @@ function App() {
 
   const [plate, setPlate] = useState([createPlate(), createPlate(), createPlate(), createPlate(), createPlate()])
   const plateImageUrl = '/plate-bg.png'
+  const [inputValue, setInputValue] = useState('')
 
   const generate = () => {
+    if(inputValue.length === 9) {
+      setPlate([inputValue, createPlate(), createPlate(), createPlate(), createPlate() ])
+      return
+    }
     setPlate([createPlate(), createPlate(), createPlate(), createPlate(), createPlate()])
   }
 
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value.toUpperCase().replace(/[^A-Z0-9]/g, '')
-    
-    if (value.length > 2) {
-      value = value.slice(0, 2) + ' ' + value.slice(2)
+    let v = e.target.value.toUpperCase().replace(/[^A-Z0-9]/g, '')
+
+    if (v.length > 2) {
+      v = v.slice(0, 2) + ' ' + v.slice(2)
     }
-    if (value.length > 5) {
-      value = value.slice(0, 5) + '-' + value.slice(5)
+    if (v.length > 5) {
+      v = v.slice(0, 5) + '-' + v.slice(5)
     }
 
-    setPlate([value, ...plate.slice(1)])
+    setInputValue(v)
+
+    if (v.length === 9) {
+      setPlate(prev => [v, ...prev.slice(1)])
+    }
   }
 
   return (
@@ -55,11 +69,19 @@ function App() {
               <h2 className="card-title text-center mb-3">Magyar rendszámtábla generátor</h2>
               <div className="mb-3 mt-4">
                 <label className="form-label text-muted small text-center mx-auto w-100">Rendszám szerkesztése</label>
+                {
+                  inputValue.length !== 9 && inputValue.length > 0 && (
+                    <div className="alert alert-danger text-center w-50 mx-auto" role="alert">
+                      Rendszám nem érvényes!
+                    </div>
+                  )
+                }
                 <input 
                   type="text" 
                   className="form-control form-control-lg text-center fw-bold text-uppercase mb-4 w-50 mx-auto" 
                   style={{ letterSpacing: '2px' }}
-                  value={plate[0]} 
+                  maxLength={9}
+                  value={inputValue}
                   onChange={handleInputChange} 
                   placeholder="AA BB-123"
                 />
